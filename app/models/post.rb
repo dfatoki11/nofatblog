ffc7e761761cefb732b2325397da27e60ec6ce6d
David Fatoki <dfatoki11@gmail.com>
2013-09-12 00:40:44 -0500

    Added devise views and created user system

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ActiveRecord::Base
-	has_many :comments
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true,
 					  length: { minimum: 5}
 	validates :author, presence: true
 	validates :text, presence: true
+
+	attr_accessible :title, :author, :text
 end
